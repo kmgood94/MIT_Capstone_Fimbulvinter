db9e6caed2e8e05bd49d3e5830c169a565198f0c
kmgood94 <kmgood94@gmail.com>
2023-08-24 17:29:39 -0600

    Moar UI Work 8.24
    Scrolls, Tabs, more.

--- Assets/QuitToDesktop.cs
+++ Assets/QuitToDesktop.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class QuitToDesktop : MonoBehaviour
 {
+   public GameObject confirmation_panel;
    public void button_exit()
    {
     Application.Quit();
+   }
+   public void enable_confirmation()
+   {
+      confirmation_panel.SetActive(true);
+   }
+   public void disable_confirmation()
+   {
+      confirmation_panel.SetActive(false);
    }
         
 }
