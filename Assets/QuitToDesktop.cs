ae9c5cc4b16cbfe41c030d792fcb70803e12c87b
kmgood94 <kmgood94@gmail.com>
2023-08-23 23:32:19 -0600

    Scene Creation and Nav work

---
+++ Assets/QuitToDesktop.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitToDesktop : MonoBehaviour
+{
+   public void button_exit()
+   {
+    Application.Quit();
+   }
+        
+}
